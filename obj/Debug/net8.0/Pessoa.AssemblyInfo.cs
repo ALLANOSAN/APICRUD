af32585d0c26792f14dd5aad3f318402e13cd9c3
ALLANOSAN <allanstryper@live.com>
2025-02-13 20:58:29 -0300

    update

--- obj/Debug/net8.0/Pessoa.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pessoa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pessoa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0199b8a05275d1a6a18ad1a3bf438e5a880d15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pessoa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pessoa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
